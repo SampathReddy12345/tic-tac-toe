5fa49b3bda3b2e28c467afc15f858b5b84b4fa62
SampathReddy12345 <sampathreddy139@gmail.com>
2020-08-18 13:18:14 +0530

    modified tactics

--- function.js
+++ function.js
@@ -1,21 +1,21 @@
-$(document).ready(function () {
+$(document).ready(function () { // preventing jQuery code from running before the document is finished loading (is ready)
 
 	var move = 1;
 	var play = true;
 
-	$("#board tr td").click(function () {
-		if ($(this).text() == "" && play) {
-			if ((move % 2) == 1) {
-				$(this).append("X");
-				$(this).css('color', "#61892f");
+	$("#board tr td").click(function () {                          // the board is being present with table row and cell
+		if ($(this).text() == "" && play) {                   //checking the text present in the cell is null(empty) or not & is able to play
+			if ((move % 2) == 1) {                       //checking for the move 
+				$(this).append("X");                //inserting x in the cell 
+				$(this).css('color', "#61892f");    // giving color for the x
 				
 			} else {
-				$(this).append("O");
-				$(this).css('color', "#e85a4f");
+				$(this).append("O");             //inserting O in the cell if the condition fails   
+				$(this).css('color', "#e85a4f");// giving color to o
 			}
-			move++;
-			if (checkForWinner() != -1 && checkForWinner() != "") {
-				if (checkForWinner() == "X") {
+			move++;                                 // incrementing the move
+			if (checkForWinner() != -1 && checkForWinner() != "") { // checking the condition for winner is not equal in a cell and is empty
+				if (checkForWinner() == "X") {       //checking the winner for x or o
 					$('body').append('<div class="winner"><span>Winner</span>X</div><button onclick="location.reload();" id="reload">Play Again</button>');
 					$('.winner').css('background-color', '#61892f');
 					$('#reload').css('color','#61892f');
@@ -29,7 +29,7 @@ $(document).ready(function () {
 		}
 	});
 
-	function checkForWinner() {
+	function checkForWinner() { // fn for  checking the winner and for each space(cell present in table) we are assigning a var 
 		var space1 = $("#board tr:nth-child(1) td:nth-child(1)").text();
 		var space2 = $("#board tr:nth-child(1) td:nth-child(2)").text();
 		var space3 = $("#board tr:nth-child(1) td:nth-child(3)").text();
@@ -61,8 +61,9 @@ $(document).ready(function () {
 		} else if ((space3 == space5) && (space5 == space7)) {
 			return space7;
 		}
-		else if(space3 && space6 && space7 &&space8 && space9){ 
-			$('body').append('<div class="winner"></span>Draw</div><button onclick="location.reload();"id="reload">Play Again</button>');
+		//checking for draw
+		else if (space1 && space2 && space3 && space4 &&  space5 && space6 && space7 && space8 && space9){
+			$('body').append('<div class="winner"><span>Draw</span></div><button onclick="location.reload();" id="reload">Play Again</button>');
 		}
 		// no winner
 		return -1;
